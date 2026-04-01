4c4702f02588ed726f85ac677af10874b419e85d
dilnazrusl <d.sama.2000@gmail.com>
2026-04-01 22:47:12 +0500

    Add Practice8 - PhoneBook with functions and stored procedures

---
+++ Prac08/config.py
@@ -0,0 +1,8 @@
+def load_config():
+    return {
+        "dbname":"phonebook_db",
+        "user":"phoneuser",
+        "password":"postgres",
+        "host":"localhost",
+        "port":"5432"
+    }
